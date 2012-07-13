adb2eb0af6248e54831528531058de3e4a1f3b20
thierry <thierry>
2012-07-13 10:53:30 +0200

    DOC: Update copyright

--- signals.c
+++ signals.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Thierry FOURNIER
+ * Copyright (c) 2008-2012 Thierry FOURNIER
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
